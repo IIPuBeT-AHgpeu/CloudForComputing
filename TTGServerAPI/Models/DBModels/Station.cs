3ef198be19f1cb94e84af20f6c68a81576a0daa7
kiran2000 <kiran2000@yandex.ru>
2022-04-29 20:19:43 +0300

    Import tables from DB in classes and create DBContext class by Npgsql.EntityFrameworkCore.PostgreSQL provider.

---
+++ TTGServerAPI/Models/DBModels/Station.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TTGServerAPI
+{
+    public partial class Station
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int WayId { get; set; }
+        public int Position { get; set; }
+        public int? Waiting { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual Way Way { get; set; } = null!;
+    }
+}
